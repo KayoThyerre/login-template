1972300413b470b5a8a5a650de50dd055ceabe0f
KayoThyerre <kayothyerrework@gmail.com>
2026-01-10 13:43:17 -0300

    Tailwind configurado e funcionando, layout de login funcionando

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,24 @@ import { PublicLayout } from "../layouts/PublicLayout";
 export function Login() {
   return (
     <PublicLayout>
-      <h1>Login</h1>
+      <div className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="E-mail"
+          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+
+        <button className="bg-blue-600 text-white rounded-md py-2 hover:bg-blue-700 transition">
+          Entrar
+        </button>
+      </div>
     </PublicLayout>
   );
 }
+
