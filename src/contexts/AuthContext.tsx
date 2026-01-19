20221a892ecfd041790e22ee5ff47d1fac3a14ca
KayoThyerre <kayothyerrework@gmail.com>
2026-01-19 16:16:52 -0300

    deu certo, parcial, descrição no próximo commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,12 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+
+type User = {
+  name: string;
+  role: string;
+};
 
 type AuthContextData = {
+  user: User | null;
   isAuthenticated: boolean;
   login: () => void;
   logout: () => void;
@@ -9,18 +15,50 @@ type AuthContextData = {
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+  const storedUser = localStorage.getItem("auth:user");
+
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+
+  // useEffect(() => {
+  //   const storedUser = localStorage.getItem("auth:user");
+
+  //   if (storedUser) {
+  //     setUser(JSON.parse(storedUser));
+  //   }
+  // }, []);
+
+
+  const isAuthenticated = !!user;
 
   function login() {
-    setIsAuthenticated(true);
+    const fakeUser = {
+      name: "Kayo Dev",
+      role: "Administrador",
+    };
+
+    setUser(fakeUser);
+    localStorage.setItem("auth:user", JSON.stringify(fakeUser));
   }
 
   function logout() {
-    setIsAuthenticated(false);
+    setUser(null);
+    localStorage.removeItem("auth:user");
   }
 
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
