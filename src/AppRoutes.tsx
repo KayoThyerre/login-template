4fc40c889fc734894f02d5f7a548c91b4ff8756e
KayoThyerre <kayothyerrework@gmail.com>
2026-01-19 17:34:18 -0300

    refatorado código de rotas

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,19 +3,47 @@ import { Login } from "../src/pages/Login";
 import { Home } from "../src/pages/Home";
 import { PrivateRoute } from "../src/routes/PrivateRoute";
 
+type AppRoute = {
+  path: string;
+  element: React.ReactNode;
+  isPrivate?: boolean;
+};
+
+const routes: AppRoute[] = [
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    isPrivate: true,
+  },
+];
+
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
+        {routes.map((route) =>
+          route.isPrivate ? (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <PrivateRoute>
+                  {route.element}
+                </PrivateRoute>
+              }
+            />
+          ) : (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.element}
+            />
+          )
+        )}
       </Routes>
     </BrowserRouter>
   );
